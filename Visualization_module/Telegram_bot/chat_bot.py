af6a5dc77a4e4f69acc382d17d464fa91fd20110
Daria Mityagina <darya.mityagina.1999@mail.ru>
2023-12-15 02:52:56 +0400

    Initial changes : added placeholders for a chatbot and streamlit + added sample phrases

---
+++ Visualization_module/Telegram_bot/chat_bot.py
@@ -0,0 +1,8 @@
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Bot token, replace with your own token
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+# To be done
